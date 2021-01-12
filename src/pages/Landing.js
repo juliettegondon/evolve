b4d14a5acb099b1769cb4197993e178def10c47d
Andrea Hayward <andrea.teofilo@yahoo.com>
2021-01-11 21:25:00 -0500

    adding API for quotes

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,34 +1,87 @@
-import React from "react";
+import React, { Component } from "react";
 import Hero from "../components/Hero";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
-import RandomQuote from "../components/RandomQuote";
+import API from "../utils/API";
 
-function Landing() {
+class Landing extends Component {
+  state = {
+    quote: "",
+  }
+
+  apiReq = () => {
+    API.getQuote().then((response) => {
+        const randNum = Math.floor(Math.random() * response.data.length)
+        const quote = response.data[randNum].text
+        console.log(quote)
+        this.setState({
+            quote: quote
+        });
+        
+    });
+  }
+  componentDidMount() {
+    this.apiReq()
+  }
+render() {
+
   return (
     <div>
+      "{this.state.quote}"
       <Hero backgroundImage="https://images.unsplash.com/photo-1609342122563-a43ac8917a3a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80">
-        <h1 className="text-center">Evolve</h1>
+        <h1>Evolve</h1>
         <h2>Connect your mind & body. </h2>
       </Hero>
       <Container style={{ marginTop: 30 }}>
         <Row>
           <Col size="md-12">
-            <h1 className="text-center">welcome, user.</h1>
+            <h1>LANDING PAGE</h1>
           </Col>
         </Row>
         <Row>
           <Col size="md-12">
-          <RandomQuote>
-            <h2>hello</h2>
-          </RandomQuote>
-            
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
+              consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
+              parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
+              sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
+              gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
+              turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
+              tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
+              quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
+              volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
+              mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
+              tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
+            </p>
+            <p>
+              Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed
+              malesuada molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi
+              ac mi hendrerit semper. Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
+              nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue dictum egestas.
+              Aliquam blandit mi eu urna scelerisque, vitae volutpat ligula ultricies. Maecenas vel
+              porta augue. Fusce mauris ex, dignissim et lacinia ut, tempus eget nibh.
+            </p>
+            <p>
+              Etiam ut massa efficitur, gravida sapien non, condimentum sapien. Suspendisse massa
+              tortor, facilisis in neque sit amet, scelerisque elementum tortor. Nullam eget nibh
+              sit amet odio lobortis ullamcorper. Nulla bibendum magna nec sem pulvinar lobortis.
+              Mauris et imperdiet urna, vitae lobortis dui. Nunc elementum elit mi, non mattis enim
+              congue at. Proin mi lectus, ullamcorper id hendrerit eu, ultricies vitae lacus. Nunc
+              vehicula, erat eget laoreet condimentum, felis ante malesuada leo, nec efficitur diam
+              nisi eget nisi. Cras arcu lacus, tristique in bibendum vitae, elementum eget lorem.
+              Maecenas vestibulum volutpat orci eu pharetra. Praesent vel blandit ante, nec faucibus
+              libero. Sed ultrices lorem ex, eu facilisis libero convallis ac. Vivamus id dapibus
+              eros. Nullam tempor sem rhoncus porta semper. Proin bibendum vulputate nisl, fringilla
+              interdum elit pulvinar eu. Quisque vitae quam dapibus, vestibulum mauris quis, laoreet
+              massa.
+            </p>
           </Col>
         </Row>
       </Container>
     </div>
   );
+}
 }
 
 export default Landing;
