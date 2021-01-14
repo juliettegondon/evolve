81721924ad2ea52b77a828bff766fd3396ef4076
Andrea Hayward <andrea.teofilo@yahoo.com>
2021-01-13 20:28:25 -0500

    adding contact page

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,23 @@
+import React from "react";
+//import Button from "react-bootstrap/Button";
+//import ButtonGroup from "react-bootstrap/ButtonGroup";
+
+const ContactUs = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h1><b>Contact Us</b></h1>
+                    <hr>
+                    <p>For website questions and help please email evolve@evolve.com</p>
+                    <hr>
+                    <p>For deployment information please visit https://github.com/juliettegondon/evolve</p>
+                    </hr>
+                    </hr>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ContactUs
