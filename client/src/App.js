05cf7a9709a5cfb941ea543d7d4d2eb18dde9851
frank salvo <fhsalvo@yahoo.com>
2021-01-20 22:36:14 -0500

    fixing merge conflict problems with API.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import VideoLibrary from "./pages/VideoLibrary";
 import Health from "./pages/Health";
 import Exercise from "./pages/Exercise";
 import ContactUs from "./pages/ContactUs";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 
 
@@ -31,6 +33,8 @@ function App() {
           <Route exact path="/health" component={Health} />
           <Route exact path="/exercise" component={Exercise} />
           <Route exact path="/contactus" component={ContactUs} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
 
         </Wrapper>
         <Footer />
